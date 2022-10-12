7023db2bc3becee25f9e166f842f2f78f9c076de
Spidw3b <jarod64@hotmail.fr>
2022-10-12 10:57:30 +0200

    first commit

---
+++ Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1Calculatrice
+{
+    internal class Class1
+    {
+        public static float Addition(float firstNumber, float secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+        public static float Soustraction(float firstNumber, float secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+        public static float Multiplication(float firstNumber, float secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+        public static float Division(float firstNumber, float secondNumber)
+        {
+            return firstNumber / secondNumber;
+        }
+        public static float Modulo(float firstNumber, float secondNumber)
+        {
+            return firstNumber % secondNumber;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using TP1Calculatrice;
+string reponse = "oui";
+while (reponse != "non")
+{
+    Console.WriteLine("Renseignez un premier nombre");
+    string entreePremierNombre = Console.ReadLine();
+    float premierNombre = float.Parse(entreePremierNombre);
+    Console.WriteLine("Renseignez un second nombre");
+    string entreeSecondNombre = Console.ReadLine();
+    float secondNombre = float.Parse(entreeSecondNombre);
+    
+    Console.WriteLine("Choisissez votre opération : + ; - ; * ; / ; %");
+    string operateur = Console.ReadLine();
+
+    if (operateur == "+")
+    {
+        Console.WriteLine(premierNombre + " + " + secondNombre + " : " + Class1.Addition(premierNombre,secondNombre));
+    }
+    else if (operateur == "-")
+    {
+        Console.WriteLine(premierNombre + " - " + secondNombre + " : " + Class1.Soustraction(premierNombre, secondNombre));
+    }
+    else if (operateur == "*")
+    {
+        Console.WriteLine(premierNombre + " x " + secondNombre + " : " + Class1.Multiplication(premierNombre, secondNombre));
+    } else if (operateur == "/") {
+        Console.WriteLine(premierNombre + " / " + secondNombre + " : " + Class1.Division(premierNombre, secondNombre));
+    } else if (operateur == "%")
+    {
+        Console.WriteLine(premierNombre + " % " + secondNombre + " : " + Class1.Modulo(premierNombre, secondNombre));
+    }
+    Console.WriteLine("Voulez vous continuer ?");
+    reponse = Console.ReadLine();
+}
